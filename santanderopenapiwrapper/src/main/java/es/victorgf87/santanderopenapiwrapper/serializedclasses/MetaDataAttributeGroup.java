81c9bb1728bf58b2a41d8d919467f67d1836adc3
victorgf87 <victor.gf87@gmail.com>
2015-07-28 17:35:47 +0200

    Commented MetaDataAttributeGroup

--- santanderopenapiwrapper/src/main/java/es/victorgf87/santanderopenapiwrapper/serializedclasses/MetaDataAttributeGroup.java
+++ santanderopenapiwrapper/src/main/java/es/victorgf87/santanderopenapiwrapper/serializedclasses/MetaDataAttributeGroup.java
@@ -5,24 +5,9 @@ import com.google.gson.internal.LinkedTreeMap;
 
 import java.util.List;
 
-/**
- * Created by Víctor on 21/07/2015.
- */
-public class MetaDataAttributeGroup
-{
 
-    @SerializedName("name")private String name;
-    @SerializedName("attributes")private Group attributes;
-
-
-
-    public String getName()
-    {
-        return name;
-    }
-
-
-    /*
+/*
+    Example of JSON hashed
     {
             "name":"icms:identifier",
             "attributes":
@@ -38,6 +23,50 @@ public class MetaDataAttributeGroup
                 }
         }
      */
+
+
+/**
+ * Created by Víctor on 21/07/2015.
+ */
+public class MetaDataAttributeGroup
+{
+
+    //Serialized names to retrieve data from dataset.
+    @SerializedName("name")private String name; //Name of the attribute group
+    @SerializedName("attributes")private Group attributes; //Group of values
+
+
+    /**
+     * Returns name of the metadata attribute
+     * @return
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+
+    
+    @Override
+    public String toString()
+    {
+        String ret="";
+        ret+="\nresolvable: "+getAttributeByName("resolvable")+"\n";
+        ret+="visible: "+getAttributeByName("visible")+"\n";
+        ret+="should_scape: "+getAttributeByName("should_scape")+"\n";
+        ret+="rule: "+getAttributeByName("rule")+"\n";
+        ret+="name: "+getAttributeByName("name")+"\n";
+        ret+="required: "+getAttributeByName("required")+"\n";
+        ret+="type: "+getAttributeByName("type")+"\n";
+        ret+="queryable: "+getAttributeByName("queryable")+"\n";
+        return ret;
+
+    }
+
+    /**
+     * Returns if it is queryable or not
+     * @return true if queryable, false if not. Null if data not found.
+     */
     public Boolean queryable()
     {
         Object o=attributes.attrs;
@@ -45,12 +74,20 @@ public class MetaDataAttributeGroup
         return ret;
     }
 
+    /**
+     * Returns string with the type of the metadata
+     * @return - name of the metadata
+     */
     public String type()
     {
         String ret= getAttributeByName("type");
         return ret;
     }
 
+    /**
+     * Tells if object is required
+     * @return - true if it's required, false otherwise. Null if data not found.
+     */
     public Boolean isRequired()
     {
         Object o=attributes.attrs;
@@ -58,12 +95,11 @@ public class MetaDataAttributeGroup
         return ret;
     }
 
-    public String name()
-    {
-        String ret= getAttributeByName("name");
-        return ret;
-    }
 
+    /**
+     * Returns rule of the object
+     * @return - string containing rule of data (Not sure of what is this, because it seems to be blank always)
+     */
     public String rule()
     {
         String ret= getAttributeByName("rule");
@@ -71,6 +107,10 @@ public class MetaDataAttributeGroup
     }
 
 
+    /**
+     * Is this object resolvable?
+     * @return - true if it is resolvable, false otherwise. Returns null if data not found.
+     */
     public Boolean isResolvable()
     {
         Object o=attributes.attrs;
@@ -78,17 +118,33 @@ public class MetaDataAttributeGroup
         return ret;
     }
 
+    /**
+     * Is this object visible?
+     * @return - true if it is visible, false otherwise. Returns null if data not found.
+     */
     public Boolean isVisible()
     {
         Boolean ret=Boolean.getBoolean(getAttributeByName("visible"));
         return ret;
     }
+
+
+    /**
+     * Should this object scape? (Don't know what this means)
+     * @return
+     */
     public Boolean should_scape()
     {
         Boolean ret=Boolean.getBoolean(getAttributeByName("should_scape"));
         return ret;
     }
 
+
+    /**
+     * Helper method that gets an attribute by its name
+     * @param name - name of the attribute to get.
+     * @return string that contains attribute value
+     */
     private String getAttributeByName(String name)
     {
         String ret=null;
@@ -110,6 +166,9 @@ public class MetaDataAttributeGroup
         return ret;
     }
 
+    /**
+     * Helper class that represents an attribute group
+     */
     private class Group
     {
         @SerializedName("attribute")private List<LinkedTreeMap<String,String>> attrs;
