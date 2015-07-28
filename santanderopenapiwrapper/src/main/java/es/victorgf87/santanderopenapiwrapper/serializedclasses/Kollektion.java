5a57b7af14499c77ec5ba98b6104e114a3e07e03
victorgf87 <victor.gf87@gmail.com>
2015-07-28 17:45:01 +0200

    Kollektion class commented

--- santanderopenapiwrapper/src/main/java/es/victorgf87/santanderopenapiwrapper/serializedclasses/Kollektion.java
+++ santanderopenapiwrapper/src/main/java/es/victorgf87/santanderopenapiwrapper/serializedclasses/Kollektion.java
@@ -6,38 +6,66 @@ import com.google.gson.internal.LinkedTreeMap;
 import java.util.List;
 
 /**
+ * This class represents a collection given by the API
  * Created by Víctor on 16/07/2015.
  */
 public class Kollektion
 {
-    @SerializedName("Id")private String id;
-    @SerializedName("title")private String title;
-    @SerializedName("author")private String author;
-    @SerializedName("listNameSpaces")private NameSpaceInCollection listNameSpaces;
+    @SerializedName("Id")private String id;         //Id of the collection
+    @SerializedName("title")private String title;   //Title of the collection
+    @SerializedName("author")private String author; //Author of the collection
+    @SerializedName("listNameSpaces")private NameSpaceInCollection listNameSpaces; //Namespaces attached to this collection.
+    @SerializedName("metadataList")private MetaData metadataList;   //Metadata list of this collection
+    @SerializedName("modifiable")private Boolean modifiable;        //Is this collection modifiable?
 
+
+    /**
+     * Gets if this collection is modifiable.
+     * @return - true if modifiable, false if not. null if data doesn't exist.
+     */
+    public Boolean isModifiable()
+    {
+        return modifiable;
+    }
+
+    /**
+     * Gets list of nameSpaces in collection
+     * @return - Object that contains the namespaces associated to this collection.
+     */
     public NameSpaceInCollection getListNameSpaces() {
         return listNameSpaces;
     }
 
+    /**
+     * Gets metadata attached to this collection.
+     * @return
+     */
     public MetaData getMetadataList() {
         return metadataList;
     }
 
-    @SerializedName("metadataList")private MetaData metadataList;
 
-    public NameSpaceInCollection getNameSpace()
-    {
-        return listNameSpaces;
-    }
-
+    /**
+     * Gets id of collection
+     * @return - String containing id of this collection
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets collection title.
+     * @return - String containing title of this collection
+     */
     public String getTitle() {
         return title;
     }
 
+
+    /**
+     * Gets author of this collection
+     * @return - String containing author of the collection
+     */
     public String getAuthor() {
         return author;
     }
@@ -48,7 +76,8 @@ public class Kollektion
         String ret="";
         ret+="Id: "+id+"\n";
         ret+="Title: "+title+"\n";
-        ret+="author: "+author;
+        ret+="author: "+author+"\n";
+        ret+="modifiable: "+modifiable+"\n";
         return ret;
     }
 }
